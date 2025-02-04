05573c8e25881894e1743c02a31518e52ba49cb1
aliabueldahab <aliabueldahab2005@gmail.com>
2025-02-04 03:24:21 -1000

    Add files via upload

---
+++ arraysjavascript/array.js
@@ -0,0 +1,72 @@
+var nums = [5 , 15 , 35 ,6 ,4 ,4 , 8 , 9]
+
+// 1- push insert a value at the end of array
+// nums.push(50)
+// console.log(nums);
+
+// 2- unshift add value to the start of array
+// nums.unshift(10)
+// console.log(nums);
+
+
+// 3- pop remove value from the end of array   
+// nums.pop(); has no parameters
+// console.log(nums);
+
+// 4- shift remove value from the start of array
+// nums.shift();
+// console.log(nums);
+
+// 5- splice add & remove element numbers in the middle of array
+// nums.splice(1 , 3)
+// console.log(nums);
+
+// 6- includes of check if that element is exist in array 
+// nums.includes(9)
+// console.log(nums); // ture or false
+
+// 7- indexof give the first index of a element in array
+// console.log(nums.indexOf(4)); answer ---> 4 ----> give (-1) if the element is dose not exist
+
+// 8-last index of give the last index of any number id it exist twice
+// console.log(nums.lastIndexOf(4)); answer ---> 5
+
+
+// 9- concat make a sum between to arrays as a numbers not as a strings
+// var arr1 = [1 , 2 , 3]
+// var arr2 = [10 , 20 , 30]
+// var x = arr1.concat(arr2)
+// console.log(x);
+
+
+// 10- if you need to empty an array 
+// 1- nums.length(0);
+// 2-nums.splice(0, nums.length())
+
+
+//11- var x = nums.every(function(checkNumber){
+//     return checkNumber > 4 // true or false for all element
+// })
+
+// console.log(x);
+
+
+//12- var x = nums.some(function(checkNumber){
+//     return checkNumber > 3 //true or false for any element 
+// })
+    // console.log(x);
+
+
+// 13- find tool use to check number if matches with another one 
+// nums.find(x > 3) // (1) if true (undefined) if false
+// console.log(nums);
+
+// 14- join make all the array is string
+// console.log(nums.join);
+
+
+
+
+
+
+
